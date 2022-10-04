9a392665be568a3671de6da1b3cb347deadceff6
ilhan enes daniş <danisenes3@gmail.com>
2022-10-04 21:29:44 +0300

    Add project files.

---
+++ RouteHandler/RouteHandler/MyRouter.cs
@@ -0,0 +1,43 @@
+﻿using RouteHandler.Service;
+
+namespace RouteHandler.RouteHandler
+{
+    public class MyRouter : IRouter
+    {
+        private readonly IRouter _router;
+        public MyRouter(IRouter router)
+        {
+            _router = router;
+           
+        }
+
+        public VirtualPathData? GetVirtualPath(VirtualPathContext context)
+        {
+            return _router.GetVirtualPath(context);
+        }
+
+        public async Task RouteAsync(RouteContext context)
+        {
+            RouteService route = new RouteService();
+            var url = context.HttpContext.Request;
+            var result = route.UrlHandler(context.HttpContext.Request.Path);
+            if (result.LastOrDefault() == "")
+            {
+                var routeHandling = route.Routes(result[0]);
+                context.RouteData.Values["controller"] = "Home";
+                context.RouteData.Values["action"] = "Index";
+                await _router.RouteAsync(context);
+            }
+            else
+            {
+                var routeHandling = route.Routes(result.LastOrDefault());
+                context.RouteData.Values["controller"] = routeHandling.Controller;
+                context.RouteData.Values["action"] = routeHandling.Action;
+                await _router.RouteAsync(context);
+            }
+          
+
+           
+        }
+    }
+}
---
+++ RouteHandler/Service/IRouteService.cs
@@ -0,0 +1,32 @@
+﻿namespace RouteHandler.Service
+{
+    public interface IRouteService
+    {
+        string[] UrlHandler(string url);
+        RoutesClass Routes(string route);
+    }
+    public class RouteService : IRouteService
+    {
+
+        public RoutesClass Routes(string route)
+        {
+            List<RoutesClass> routes = new List<RoutesClass>()
+            {
+                new RoutesClass(){Action="Index",Controller="Home",Slug="/"},
+                new RoutesClass(){Action="Privacy",Controller="Home",Slug="Hakkimizda"},
+            };
+            return routes.Where(x => x.Slug == route).FirstOrDefault();
+        }
+
+        public string[] UrlHandler(string url)
+        {
+           return url.Split('/');
+        }
+    }
+    public class RoutesClass
+    {
+        public string Slug { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
